b24db824cb573b2e0638bbd6f379e1f0ed1f0dd7
victorgh31 <victor.bertrand31@gmail.com>
2024-03-17 20:29:30 +0100

    TP2-4 : Cas PrendreEtal, la frontière BoundaryPrendreEtal est compétée.

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,29 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (controlPrendreEtal.verifierIdentite(nomVendeur)) {
+            System.out.println("L'identité du vendeur est vérifiée.");
+            if (controlPrendreEtal.resteEtals()) {
+                System.out.println("Il reste des étals disponibles dans le village.");
+                System.out.print("Entrez le produit à vendre : ");
+                String produit = scan.nextLine();
+                System.out.print("Entrez le nombre de produits à vendre : ");
+                int nbProduit = scan.nextInt();
+                int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+                if (numeroEtal != -1) {
+                    System.out.println("L'étal a été attribué au vendeur avec succès. Numéro de l'étal : " + numeroEtal);
+                } else {
+                    System.out.println("Impossible d'attribuer un étal au vendeur. Veuillez réessayer plus tard.");
+                }
+            } else {
+                System.out.println("Tous les étals sont occupés pour le moment. Veuillez réessayer plus tard.");
+            }
+        } else {
+            System.out.println("L'identité du vendeur n'a pas pu être vérifiée. Impossible de prendre un étal.");
+        }
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		prendreEtal(nomVendeur);
 	}
 }
