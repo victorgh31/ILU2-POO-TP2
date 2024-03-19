335197fd2cb26d1bfe849b9cd0e0c426ef4a2bdb
victorgh31 <victor.bertrand31@gmail.com>
2024-03-19 19:35:40 +0100

    TP2-7 : Cas Afficher Marché, la frontière BoundaryAfficherMarche est complétée.

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,17 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
-	}
+        String[] infosEtals = controlAfficherMarche.afficherEtalsMarche();
+        if (infosEtals.length > 0) {
+            System.out.println("Voici les étals disponibles sur le marché :");
+            for (int i = 0; i < infosEtals.length; i += 3) {
+                String nomVendeur = infosEtals[i];
+                int quantiteRestante = Integer.parseInt(infosEtals[i + 1]);
+                String produit = infosEtals[i + 2];
+                System.out.println("Vendeur : " + nomVendeur + ", Produit : " + produit + ", Quantité restante : " + quantiteRestante);
+            }
+        } else {
+            System.out.println("Il n'y a aucun étal disponible sur le marché pour le moment.");
+        }
+    }
 }
