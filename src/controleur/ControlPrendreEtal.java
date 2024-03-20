f029ece7405f71f17e3ea0a0168abb55f3162355
victorgh31 <victor.bertrand31@gmail.com>
2024-03-20 12:52:11 +0100

    ILU2-POO-TP5 : Le cas PrendreEtal est enfin opérationnel, testé avec le scénario présenté.

--- src/controleur/ControlPrendreEtal.java
+++ src/controleur/ControlPrendreEtal.java
@@ -1,6 +1,7 @@
 package controleur;
 
 import frontiere.Clavier;
+import personnages.Gaulois;
 import villagegaulois.Village;
 
 public class ControlPrendreEtal {
@@ -18,6 +19,7 @@ public class ControlPrendreEtal {
 		return village.rechercherEtalVide();
 	}
 
+/*	
 	public boolean prendreEtal(String nomVendeur, String produit, int nbProduit) {
 		// On prend un étal si l'identité du vendeur est vérifiée
         if (!controlVerifierIdentite.verifierIdentite(nomVendeur)) {      	
@@ -26,7 +28,13 @@ public class ControlPrendreEtal {
     		return true;
 	    }
 	}
-
+*/
+	
+	public int prendreEtal(String nomVendeur, String produit, int nbProduit) {
+		Gaulois gaulois = village.trouverHabitant(nomVendeur);
+		return village.installerVendeur(gaulois, produit, nbProduit);
+	}
+	
 	public boolean verifierIdentite(String nomVendeur) {
 		// On vérifie l'identité du vendeur à l'aide du contrôleur ControlVerifierIdentite
         return controlVerifierIdentite.verifierIdentite(nomVendeur);
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -6,32 +6,42 @@ import controleur.ControlPrendreEtal;
 
 public class BoundaryPrendreEtal {
 	private ControlPrendreEtal controlPrendreEtal;
-	private Scanner scan = new Scanner(System.in);
+	private Scanner scanner;
 
 	public BoundaryPrendreEtal(ControlPrendreEtal controlChercherEtal) {
 		this.controlPrendreEtal = controlChercherEtal;
+		scanner = new Scanner(System.in);
 	}
 
 	public void prendreEtal(String nomVendeur) {
 		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {     	
-			System.out.println("Bonjour " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+			System.out.println("Je suis désolé " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
 		} else {
 	    	System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.");
 	    	
 	    	if (!controlPrendreEtal.resteEtals()) {
 	    		System.out.println("Désolé " + nomVendeur + " je n'ai plus d'étal qui ne soit pas déjà occupé.");
 	    	} else {
-	    		System.out.println("C'est parfait, il me reste un étal pour vous !");
-	    		System.out.println("Il me faudrait quelques renseignements :");
-	    		System.out.println("Quel produit souhaitez-vous vendre ?");
-	    		//clavuer
-	    		System.out.println("Combien souhaitez-vous en vendre ?");
+	    		installerVendeur(nomVendeur);
 	    	}
 		}	
             
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		prendreEtal(nomVendeur);
+		System.out.println("C'est parfait, il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements :");
+		
+		System.out.println("Quel produit souhaitez-vous vendre ?");	
+		String produit = scanner.nextLine();
+		
+		System.out.println("Combien souhaitez-vous en vendre ?");
+		int nbProduit = scanner.nextInt();
+		
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+		
+		if (numeroEtal != -1) {
+			System.out.println("Le vendeur " + nomVendeur + " s'est installé à l'étal n°" + numeroEtal);
+		}
 	}
 }
