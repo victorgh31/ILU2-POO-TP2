02c06501fb53b3b6e95b0ba0b369e3ac6b4579a5
victorgh31 <victor.bertrand31@gmail.com>
2024-04-05 13:55:29 +0200

    ILU2-TEST-TP2 : Création de tests pour la classe ControlAcheterProduitTest. Fail sur le testDonnerVendeurs. A corriger.

---
+++ test/controleur/ControlAcheterProduitTest.java
@@ -0,0 +1,63 @@
+
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Gaulois;
+import villagegaulois.Village;
+
+class ControlAcheterProduitTest {
+	private Village village;
+	private Chef abraracourcix;
+	private Gaulois vendeur;
+	
+	@BeforeEach
+	public void controlAcheterProduit () {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		vendeur = new Gaulois("Bonemine", 3);
+		village.ajouterHabitant(vendeur);
+		village.installerVendeur(vendeur, "fleurs", 10);
+	}
+	
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		ControlAcheterProduit controlAcheterProduit = new ControlAcheterProduit(
+				controlVerifierIdentite, controlTrouverEtalVendeur, village);
+		assertTrue(controlAcheterProduit.verifierIdentite("Bonemine"));
+		assertFalse(controlAcheterProduit.verifierIdentite("Asterix"));
+		
+	}
+	
+	@Test
+	void testDonnerVendeurs() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		ControlAcheterProduit controlAcheterProduit = new ControlAcheterProduit(
+				controlVerifierIdentite, controlTrouverEtalVendeur, village);
+		Gaulois[] vendeurs = controlAcheterProduit.donnerVendeurs("fleurs");
+		assertEquals(1, vendeurs.length);
+		assertEquals(vendeur.getNom(), vendeurs[0]);
+	}
+	
+	@Test
+	void testAcheterProduit() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		ControlTrouverEtalVendeur controlTrouverEtalVendeur = new ControlTrouverEtalVendeur(village);
+		ControlAcheterProduit controlAcheterProduit = new ControlAcheterProduit(
+				controlVerifierIdentite, controlTrouverEtalVendeur, village);
+		assertEquals(2, controlAcheterProduit.quantiteAchetee(vendeur.getNom(), 2));
+		assertEquals(8, controlAcheterProduit.quantiteAchetee(vendeur.getNom(), 9));
+		assertEquals(0, controlAcheterProduit.quantiteAchetee(vendeur.getNom(), 2));
+		
+	}
+
+}
